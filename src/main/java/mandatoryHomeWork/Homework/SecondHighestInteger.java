49098a883b3e9aa6e1e14e8214c7b72d89d042ce
AswinBahulayan <aswinbahulayan@gmail.com>
2023-08-05 20:34:01 +0530

    Added solution for second highest int without set

--- src/main/java/mandatoryHomeWork/Homework/SecondHighestInteger.java
+++ src/main/java/mandatoryHomeWork/Homework/SecondHighestInteger.java
@@ -15,6 +15,11 @@ public class SecondHighestInteger {
         Assertions.assertEquals(94,secondHighestInteger(new int[]{100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89}));
     }
 
+    @Test
+    public void test2(){
+        Assertions.assertEquals(94,secondHighestIntegerWithOutSet(new int[]{100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89}));
+    }
+
     public int secondHighestInteger(int[] arr){
         Set<Integer> set=new TreeSet<>();
         for(int i=0;i<arr.length;i++){
@@ -27,5 +32,19 @@ public class SecondHighestInteger {
             i++;
         }
         return array[array.length-2];
+    }
+
+    public int secondHighestIntegerWithOutSet(int[] arr){
+        int temp=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                    if(arr[i]>arr[j]){
+                        temp=arr[i];
+                        arr[i]=arr[j];
+                        arr[j]=temp;
+                    }
+            }
+        }
+        return arr[arr.length-2];
     }
 }
