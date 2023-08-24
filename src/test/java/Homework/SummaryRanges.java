95c7ee677f2baec66c012478c27935d7323913b1
AswinBahulayan <aswinbahulayan@gmail.com>
2023-08-24 22:28:26 +0530

    Added solution for homework problems summary range

--- src/test/java/Homework/SummaryRanges.java
+++ src/test/java/Homework/SummaryRanges.java
@@ -27,6 +27,15 @@ public class SummaryRanges {
         Assert.assertEquals(Arrays.asList("-2->-1","1->2","2147483646->2147483647"),summaryRanges(new int[]{-2,-1,1,2,2147483646,2147483647}));
     }
 
+    /*
+    * 1.input is an array
+    * 2.output is a list<String>
+    * 3.declare a List<String> to store the summary range
+    * 4.declare a int to find the diff between arr[i] and [i+1]
+    * 5.if difference is equal to 1 concatenate arr[i] and arr[i+1] using a ";" as separator
+    * 6.if difference is not equal to 1 add the string with concatenated array elements to the list
+    * and reset the string to null, add arr[i+1] to list
+    * 7.finally after all the are added remove repeated elements from the list*/
     public List<String> summaryRanges(int[] nums) {
         if(nums.length==1){
             return Arrays.asList(String.valueOf(nums[0]));
