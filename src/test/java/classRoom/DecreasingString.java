24adc54c65ca4f6550cc96e77e6bba8df93a2141
AswinBahulayan <aswinbahulayan@gmail.com>
2023-09-06 21:06:06 +0530

    Added solution for decreasing string

--- src/test/java/classRoom/DecreasingString.java
+++ src/test/java/classRoom/DecreasingString.java
@@ -15,23 +15,23 @@ public class DecreasingString {
      3.create a an array with length 128
      4.iterate the string and store the count of each character in appropriate index (ASCII value of each character)
      5.using the count of each chracter stored in array create a temporary string to store the charcters in ascending and descending order until the count of charcter is zero
-     6.time complexity is o[n*m]*/
+     6.time complexity is o[n+m]*/
     public String sortString(String s) {
         int[] arr = new int[128];
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {//o[n]
+        for (int i = 0; i < s.length(); i++){//o[n]
             arr[s.charAt(i)]+=1;
         }
         int count = s.length();
         while (count > 0) {//o[m]
-            for (int i = 96; i < arr.length; i++) {//o[n]
+            for (int i = 96; i < 123; i++) {//o[1]
                 if (arr[i] != 0) {
                     output.append((char)i);
                     arr[i]--;
                     count--;
                 }
             }
-            for (int i = arr.length - 1; i > 96; i--) {//o[n]
+            for (int i = 123; i > 96; i--) {//o[1]
                 if (arr[i] != 0) {
                     output.append((char)i);
                     arr[i]--;
