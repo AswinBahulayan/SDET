3084953bee58bfdfe8b85b18fd6f65e3842ee0fa
AswinBahulayan <aswinbahulayan@gmail.com>
2023-09-06 20:31:02 +0530

    Added solution for decreasing string

---
+++ src/test/java/classRoom/DecreasingString.java
@@ -0,0 +1,43 @@
+package classRoom;
+
+import org.junit.Test;
+
+public class DecreasingString {
+
+    @Test
+    public void test(){
+        System.out.println(sortString("aaaabbbbcccc"));
+    }
+
+    /**
+     1.input is a string
+     2.output is a string
+     3.create a an array with length 128
+     4.iterate the string and store the count of each character in appropriate index (ASCII value of each character)
+     5.using the count of each chracter stored in array create a temporary string to store the charcters in ascending and descending order until the count of charcter is zero */
+    public String sortString(String s) {
+        int[] arr = new int[128];
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {//o[n]
+            arr[s.charAt(i)]+=1;
+        }
+        int count = s.length();
+        while (count > 0) {
+            for (int i = 96; i < arr.length; i++) {
+                if (arr[i] != 0) {
+                    output.append((char)i);
+                    arr[i]--;
+                    count--;
+                }
+            }
+            for (int i = arr.length - 1; i > 96; i--) {
+                if (arr[i] != 0) {
+                    output.append((char)i);
+                    arr[i]--;
+                    count--;
+                }
+            }
+        }
+        return output.toString();
+    }
+}
