d7f276cb7933c0ef42f77898aac2a83f516521c8
AswinBahulayan <aswinbahulayan@gmail.com>
2023-09-08 21:17:58 +0530

    Added solution for Sorting sentence

--- src/test/java/classRoom/SortingTheSentence.java
+++ src/test/java/classRoom/SortingTheSentence.java
@@ -19,12 +19,13 @@ public class SortingTheSentence {
      1.input is a strring
      2.output is a string
      3.split the string using space and add to an array
-     4.Using the last char of each element in array append to the stringbuilder excluding the last character of each string*/
+     4.Using the last char of each element in array append to the stringbuilder excluding the last character of each string
+     5. time complexity o[n*m]*/
     public static String sortSentence(String s) {
-        String[] input=s.split("\\s");
+        String[] input=s.split("\\s");//o[n]
         StringBuilder output=new StringBuilder();
-        for(int i=1;i<=input.length;i++){
-            for (String string : input) {
+        for(int i=1;i<=input.length;i++){//o[n]
+            for (String string : input) {//o[m]
                 if (Character.getNumericValue(string.charAt(string.length() - 1)) == i) {
                     output.append(string.replaceAll("[0-9]", ""));
                     output.append((char) 32);
